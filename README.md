9064ac794c559c91e6927d05f111db4d3c0a8183
Michal Rogozinski <mjrogozinski@gmail.com>
2025-04-21 11:37:18 +0200

    first working setup with unit tests and lsp

---
+++ README.md
@@ -0,0 +1,2 @@
+This is to train unit tests in lua with chatgpt
+https://chatgpt.com/c/68055a55-83bc-8009-aa96-73d75b9a2d75
---
+++ tests/my_module_spec.lua
@@ -0,0 +1,22 @@
+local my_module = require("my_module")
+local eq = assert.equals
+
+describe("my_module", function()
+	it("adds numbers correctly", function()
+		local result = my_module.add(2, 3)
+		eq(result, 5)
+	end)
+
+	it("handles zero", function()
+		local result = my_module.add(0, 0)
+		eq(result, 0)
+	end)
+
+	it("just works", function()
+		eq(my_module.add(1, 1), 2)
+	end)
+
+	it("subtracts", function()
+		eq(my_module.sub(1, 1), 0)
+	end)
+end)
